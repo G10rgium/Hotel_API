ef8faa260e2eed4346a72f80d69316c0aae12491
Giorgi Khitarishvili <giorgi.khitarishvili@outlook.com>
2024-10-25 18:26:45 +0400

    idk

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
-let apiURL = "https://restaurant.stepprojects.ge/api/Categories/GetAll"
+let cardArea = document.querySelector(".API");
 
+let apiURL = "https://restaurant.stepprojects.ge/api/Categories/GetAll";
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((response) => intoHtml(response));
+
+
+function intoHtml(response) {
+    console.log(response);
+
+
+    response.forEach((info) => {
+
+        cardArea.innerHTML += `
+    <div class="card" style="width: 18rem;">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title"></h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+    `;
+    });
+}
